d361d9b82266bd77b282c0accc02efb50285ad9e
Jake Shams <jshams62@gmail.com>
2020-02-27 11:30:47 -0800

    added comparison magic method for kmeans comparisons

--- user.py
+++ user.py
@@ -1,12 +1,4 @@
-'''
-### Create Users
-- From the data we are going to create a bunch of user objects.
-- These users will have an ID and they will store the movies they've rated.
-- User objects can also be compared, and a similarity score is computed.
-  - that metric can be decided on later
-    - euclidian distance
-    - cosine similarty
-'''
+from math import sqrt
 
 
 class User:
@@ -14,6 +6,27 @@ class User:
         '''initialize a user object'''
         self._id = _id
         self.movie_ratings = {}
+
+    def __eq__(self, other):
+        return self._id == other._id
+
+    def __ne__(self, other):
+        return self._id != other._id
+
+    def __gt__(self, other):
+        return self._id > other._id
+
+    def __ge__(self, other):
+        return self._id <= other._id
+
+    def __lt__(self, other):
+        return self._id < other._id
+
+    def __le__(self, other):
+        return self._id <= other._id
+
+    def __hash__(self):
+        return hash(self._id)
 
     def add_movie_rating(self, movie_id, rating):
         '''add a movie and its rating to self'''
