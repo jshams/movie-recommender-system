c9d78b16614af3f504d5d5348d80b75af7bcba16
Jake Shams <jshams62@gmail.com>
2020-03-01 22:05:42 -0800

    movies class can now get titles from ids and ids from titles

--- movies.py
+++ movies.py
@@ -2,6 +2,7 @@ class Movies:
     def __init__(self):
         '''initialize a movies class with a dict to store the movies by id'''
         self.movies = {}
+        self.titles = {}
 
     def get_title(self, movie_id):
         '''given a movie_id return the title associated with it'''
@@ -9,7 +10,13 @@ class Movies:
             return None
         return self.movies[movie_id]
 
+    def get_id(self, title):
+        if title not in self.titles:
+            return None
+        return self.titles[title]
+
     def add_movie(self, movie_id, title):
         '''add a movie to movies dictionary. key=movie_id, val=title'''
         if movie_id not in self.movies:
             self.movies[movie_id] = title
+            self.titles[title] = movie_id
